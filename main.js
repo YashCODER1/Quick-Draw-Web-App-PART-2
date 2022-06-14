2f4c51acd55e78ee9983e1e05a4f9b51d6c6606e
YashCODER1 <94070528+YashCODER1@users.noreply.github.com>
2022-06-14 16:57:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+
+}
+function setup()
+{
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function draw()
+{
+    
+}
+function clear_canvas()
+{
+    background("White");
+}
